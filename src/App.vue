ceafe3cd02484e78bef4736d6d494c921b5063b7
dogak <dogakupa@gmail.com>
2024-04-01 16:28:13 +0300

    vuex non-api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div id="app">
+  <p>{{$store.state.sayac}}</p>
+  <button @click="artir">artır </button>
+  <button @click="artirMutations">artır </button>
+  <button @click="artiractionİle">action </button>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  <p>sayacı göster {{$store.getters.getCounter}}</p>
+  <p>sayacı sıfırla  {{$store.getters.sayacIncrements}}</p>
+  
+  </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'App',
+    methods : {
+      artir() {
+        this.$store.state.sayac++;
+      },
+      artirMutations(){
+        this.$store.commit('artirParametreile', 10)
+      },
+      artiractionİle(){
+        this.$store.dispatch('stateIncrements', 20)
+      }
+    }
+    
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,42 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createStore} from "vuex";
 
-createApp(App).mount('#app')
+const store = createStore({
+    //container 
+    state() {
+        return {
+            sayac : 0
+        }     
+    },
+    mutations : {
+        artirParametreile(state, payload){
+            state.sayac = state.sayac + payload;
+        }
+    },
+    getters :{
+        getCounter(state){
+            return state.sayac
+        },
+        sayacIncrements(state, getter){
+            if(getter.getCounter >= 50){
+                state = 10;
+                return 10;
+            }
+            return getter.getCounter;
+        }
+    },
+    action : {
+        stateIncrements(context){
+            context.commit('artirParametreile', 20)
+        }
+    }
+})
+
+//createApp(App).mount('#app')
+const app = createApp(App)
+
+// use the store in the App
+app.use(store)
+
+app.mount('#app')
